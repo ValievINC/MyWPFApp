2ba2c61ba28887ffc6043ae6ab7aa1046dabf3c6
Bulat Valiev <gennadypetrenko228@gmail.com>
2025-04-04 19:01:17 +0500

    Some Fields Added

--- Person.cs
+++ Person.cs
@@ -11,6 +11,18 @@ namespace MyWPFApp
         public string Name { get; set; }
         public int Age { get; set; }
 
+        public string City { get; set; }
+
+        public string Region { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Fax { get; set; }
+
         public Person(string name, int age)
         {
             Name = name;
